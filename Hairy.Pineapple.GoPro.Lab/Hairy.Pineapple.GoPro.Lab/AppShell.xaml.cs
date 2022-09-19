0bbcd09c9b0d6a7f07f2280b106509cae539ce85
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-19 16:51:46 +0100

    Initial commit of Hairy.Pineapple.GoPro.Lab project files.

---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Hairy.Pineapple.GoPro.Lab;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Hairy.Pineapple.GoPro.Lab;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

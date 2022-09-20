2bc7655597f5d680ffcaa1d27d0ab4fb3a26eaec
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-20 20:33:14 +0100

    Adding test EF integration code (for testing only).
    Now I have the project stubbed out I can build it out from here.

---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Configuration/PresetHeaderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Configuration
+{
+    public class PresetHeaderConfiguration : IEntityTypeConfiguration<PresetHeader>
+    {
+        public void Configure(EntityTypeBuilder<PresetHeader> builder)
+        {
+            builder.HasKey(presetHeader => presetHeader.Id);
+
+            builder
+                .HasIndex(presetHeader => presetHeader.UniqueId)
+                .IsUnique();
+        }
+    }
+}
--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Context/GoProLabDbContext.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Context/GoProLabDbContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Context
 {
     public class GoProLabDbContext : DbContext
     {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

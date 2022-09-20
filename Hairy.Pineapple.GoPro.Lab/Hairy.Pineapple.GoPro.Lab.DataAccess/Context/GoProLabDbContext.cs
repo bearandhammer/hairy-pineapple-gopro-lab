8ed4bfe774fe0fc40ca7d1a52bacbd46e76a228f
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-20 20:38:15 +0100

    Context tweak to add DataSource.
    Just test code at this stage.

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Context/GoProLabDbContext.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Context/GoProLabDbContext.cs
@@ -5,11 +5,12 @@ namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Context
 {
     public class GoProLabDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite("DataSource=test.db");
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-
-            base.OnModelCreating(modelBuilder);
         }
     }
 }

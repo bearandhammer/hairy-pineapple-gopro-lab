8b1731ca2877f7bcc0123865a1dc2c7021862b58
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-20 20:46:52 +0100

    Preparing for migration.
    Not 100% sure if I will have to shift project types.

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/MauiProgram.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace Hairy.Pineapple.GoPro.Lab;
+﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Context;
+
+namespace Hairy.Pineapple.GoPro.Lab;
 
 public static class MauiProgram
 {
@@ -12,6 +14,8 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services.AddDbContext<GoProLabDbContext>();
 
 		return builder.Build();
 	}

52fc188e5c9fb777e8b9c646fe897d8285243696
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-24 08:33:45 +0100

    Recreate the PresetHeader model, for testing.

---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess.Entities/Models/PresetHeader.cs
@@ -0,0 +1,13 @@
+﻿namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Entities.Models
+{
+    public class PresetHeader
+    {
+        public int Id { get; set; }
+
+        public Guid UniqueId { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}

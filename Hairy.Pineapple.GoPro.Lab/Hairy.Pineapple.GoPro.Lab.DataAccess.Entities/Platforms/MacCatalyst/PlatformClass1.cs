b55d4473f4139c1b612b4c0aed17507f9b248ba6
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-24 08:16:49 +0100

    Starting again with solution/ef.
    Project load issues (plus cannot target MAUI Lib with EF tooling, known issue).

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess.Entities/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Entities
-{
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1
-    {
-    }
-}
--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Hairy.Pineapple.GoPro.Lab.DataAccess
-{
-    // All the code in this file is only included on Windows.
-    public class PlatformClass1
-    {
-    }
-}

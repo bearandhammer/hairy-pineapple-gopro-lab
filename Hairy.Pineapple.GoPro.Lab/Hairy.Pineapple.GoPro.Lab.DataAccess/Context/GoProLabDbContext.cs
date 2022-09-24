a1ce0e755ff6c18f118bb5a77e6f4a4ae4ec08a2
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-24 09:48:01 +0100

    Proof of concept.

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Context/GoProLabDbContext.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Context/GoProLabDbContext.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Entities.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Context
 {
     public class GoProLabDbContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-            optionsBuilder.UseSqlite("DataSource=test.db");
+        public GoProLabDbContext()
+        {
+        }
+
+        public GoProLabDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<PresetHeader> PresetHeaders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var sqlitePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"GoProLab\Data1");
+            Directory.CreateDirectory(sqlitePath);
+            var fileName = $"{sqlitePath}\test.db";
+            if (!File.Exists(fileName))
+                File.Create(fileName);
+
+            optionsBuilder.UseSqlite($"Data Source={fileName}", options =>
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name));
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

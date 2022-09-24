06828af29db900f5f2d183cc9abb47faa2c083d7
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-24 08:39:04 +0100

    Fixing EF (basic) implementation.

---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Context/GoProLabDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Context
+{
+    public class GoProLabDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite("DataSource=test.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}

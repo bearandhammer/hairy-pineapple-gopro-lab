2703e7d30b56169da9e707728a4bf96b86b4ffce
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-24 08:50:24 +0100

    DbContext initial migration added.

---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Migrations/20220924074856_InitialMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PresetHeader",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UniqueId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PresetHeader", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PresetHeader_UniqueId",
+                table: "PresetHeader",
+                column: "UniqueId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PresetHeader");
+        }
+    }
+}

d1be30cc1b7766f288a72dbc530c77900a300e08
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-25 17:13:38 +0100

    Test changes.

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/MauiProgram.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/MauiProgram.cs
@@ -7,22 +7,22 @@ namespace Hairy.Pineapple.GoPro.Lab;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		builder.Services.AddDbContext<GoProLabDbContext>();
-		builder.Services.AddSingleton<IPresetHeaderRepo, PresetHeaderRepo>();
-		builder.Services.AddSingleton<MainPageViewModel>();
-		builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddDbContext<GoProLabDbContext>();
+        builder.Services.AddTransient<IPresetHeaderRepo, PresetHeaderRepo>();
+        builder.Services.AddTransient<MainPageViewModel>();
+        builder.Services.AddTransient<MainPage>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }

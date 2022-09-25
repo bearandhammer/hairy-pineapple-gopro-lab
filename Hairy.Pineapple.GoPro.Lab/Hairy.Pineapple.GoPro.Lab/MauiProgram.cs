e75649230298b203943aa49b502ad335c95d13e1
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-25 12:40:24 +0100

    Configure services.
    Still some work to do to consider correct scoping (seem mixed documentation on best practice for app environments).

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/MauiProgram.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Context;
+using Hairy.Pineapple.GoPro.Lab.DataAccess.Interfaces;
+using Hairy.Pineapple.GoPro.Lab.DataAccess.Repos;
+using Hairy.Pineapple.GoPro.Lab.ViewModels;
 
 namespace Hairy.Pineapple.GoPro.Lab;
 
@@ -16,6 +19,8 @@ public static class MauiProgram
 			});
 
 		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddTransient<MainPageViewModel>();
+		builder.Services.AddTransient<IPresetHeaderRepo, PresetHeaderRepo>();
 		builder.Services.AddDbContext<GoProLabDbContext>();
 
 		return builder.Build();

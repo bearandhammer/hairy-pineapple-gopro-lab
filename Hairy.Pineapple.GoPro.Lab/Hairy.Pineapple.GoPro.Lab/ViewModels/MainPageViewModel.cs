24db40396f375c02272abba1fd2b357305369fd8
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-25 13:03:46 +0100

    Quick DI end-to-end. DI of repo to fix.

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/ViewModels/MainPageViewModel.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Entities.Models;
+﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Context;
+using Hairy.Pineapple.GoPro.Lab.DataAccess.Entities.Models;
+using Hairy.Pineapple.GoPro.Lab.DataAccess.Interfaces;
 using Hairy.Pineapple.GoPro.Lab.DataAccess.Repos;
 using Hairy.Pineapple.GoPro.Lab.ViewModels.Base;
 using System.Collections.ObjectModel;
@@ -7,28 +9,44 @@ namespace Hairy.Pineapple.GoPro.Lab.ViewModels
 {
     public class MainPageViewModel : BaseViewModel
     {
-        private readonly PresetHeaderRepo presetHeaderRepo;
+        //private readonly PresetHeaderRepo presetHeaderRepo;
 
         // TODO - change to a domain model
         public ObservableCollection<PresetHeader> PresetHeaders { get; set; } = new ObservableCollection<PresetHeader>();
 
-        public MainPageViewModel(PresetHeaderRepo presetHeaderRepoType)
-        {
-            presetHeaderRepo = presetHeaderRepoType;
+        public int PresetHeaderCount => PresetHeaders.Count;
 
+        //public MainPageViewModel(PresetHeaderRepo presetHeaderRepoType)
+        public MainPageViewModel()
+        {
             LoadPresetHeaders();
         }
 
         private async void LoadPresetHeaders()
         {
-            IEnumerable<PresetHeader> presetHeaders = await presetHeaderRepo.GetAllPresetHeadersAsync();
-
-            if (presetHeaders is not null)
+            try
             {
-                foreach (PresetHeader presetHeaderItem in presetHeaders)
+                // TODO: Remove temp code and fix DI
+                using (GoProLabDbContext appDbContext = new GoProLabDbContext())
                 {
-                    PresetHeaders.Add(presetHeaderItem);
+                    IPresetHeaderRepo repo = new PresetHeaderRepo(appDbContext);
+
+                    IEnumerable<PresetHeader> presetHeaders = await repo.GetAllPresetHeadersAsync();
+
+                    if (presetHeaders is not null)
+                    {
+                        foreach (PresetHeader presetHeaderItem in presetHeaders)
+                        {
+                            PresetHeaders.Add(presetHeaderItem);
+                        }
+
+                        OnPropertyChanged(nameof(PresetHeaders));
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }

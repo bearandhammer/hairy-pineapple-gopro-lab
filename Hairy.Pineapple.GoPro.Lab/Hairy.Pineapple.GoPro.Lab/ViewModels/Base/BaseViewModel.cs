0d55192f90219088fd5114ce249e83a1b95cdf4d
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-25 12:32:36 +0100

    Add basic abstract BaseViewModel.

---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hairy.Pineapple.GoPro.Lab.ViewModels.Base
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}

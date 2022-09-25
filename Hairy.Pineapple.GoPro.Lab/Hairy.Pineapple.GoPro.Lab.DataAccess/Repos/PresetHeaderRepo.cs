7af9f1a1431062c42b2c885ba759553a130baab9
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-25 12:17:29 +0100

    Stub initial interface methods for the PresetHeader repository.

--- Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Repos/PresetHeaderRepo.cs
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab.DataAccess/Repos/PresetHeaderRepo.cs
@@ -1,8 +1,31 @@
-﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Interfaces;
+﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Context;
+using Hairy.Pineapple.GoPro.Lab.DataAccess.Entities.Models;
+using Hairy.Pineapple.GoPro.Lab.DataAccess.Interfaces;
 
 namespace Hairy.Pineapple.GoPro.Lab.DataAccess.Repos
 {
     public class PresetHeaderRepo : IPresetHeaderRepo
     {
+        private readonly GoProLabDbContext appDbContext;
+
+        public PresetHeaderRepo(GoProLabDbContext dbContextType)
+        {
+            appDbContext = dbContextType;
+        }
+
+        public Task<PresetHeader> DeletePresetHeaderByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PresetHeader>> GetAllPresetHeadersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PresetHeader> GetPresetHeaderByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

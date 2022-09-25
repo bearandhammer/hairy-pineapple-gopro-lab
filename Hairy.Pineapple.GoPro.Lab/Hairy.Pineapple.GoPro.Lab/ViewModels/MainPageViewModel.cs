0e6d669c26393af6755788304092af95e8e49c3f
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-25 12:38:38 +0100

    Super basic View Model for the main page.

---
+++ Hairy.Pineapple.GoPro.Lab/Hairy.Pineapple.GoPro.Lab/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Hairy.Pineapple.GoPro.Lab.DataAccess.Entities.Models;
+using Hairy.Pineapple.GoPro.Lab.DataAccess.Repos;
+using Hairy.Pineapple.GoPro.Lab.ViewModels.Base;
+using System.Collections.ObjectModel;
+
+namespace Hairy.Pineapple.GoPro.Lab.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private readonly PresetHeaderRepo presetHeaderRepo;
+
+        // TODO - change to a domain model
+        public ObservableCollection<PresetHeader> PresetHeaders { get; set; } = new ObservableCollection<PresetHeader>();
+
+        public MainPageViewModel(PresetHeaderRepo presetHeaderRepoType)
+        {
+            presetHeaderRepo = presetHeaderRepoType;
+
+            LoadPresetHeaders();
+        }
+
+        private async void LoadPresetHeaders()
+        {
+            IEnumerable<PresetHeader> presetHeaders = await presetHeaderRepo.GetAllPresetHeadersAsync();
+
+            if (presetHeaders is not null)
+            {
+                foreach (PresetHeader presetHeaderItem in presetHeaders)
+                {
+                    PresetHeaders.Add(presetHeaderItem);
+                }
+            }
+        }
+    }
+}
